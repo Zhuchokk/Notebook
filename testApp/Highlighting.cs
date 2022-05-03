7841fb390f34dc20c835547f85d698cf06ffe0b8
Artiom <azhuchok207@gmail.com>
2022-05-04 01:46:07 +0300

    regex added

--- testApp/Highlighting.cs
+++ testApp/Highlighting.cs
@@ -16,23 +16,24 @@ namespace testApp
         {
             using (StreamReader reader = new StreamReader(Application.StartupPath + @"\dictionaries\py.txt"))
             {
-                pykeys = "(" + reader.ReadToEnd().Replace("\r\n", " |");
+                pykeys = "(" + reader.ReadToEnd().Replace("\r\n", "|");
 
                 Console.WriteLine(pykeys);
             }
             using (StreamReader reader = new StreamReader(Application.StartupPath + @"\dictionaries\cpp.txt"))
             {
-                cppkeys = "(" + reader.ReadToEnd().Replace("\r\n", " |");
+                cppkeys = "(" + reader.ReadToEnd().Replace("\r\n", "|");
             }
             using (StreamReader reader = new StreamReader(Application.StartupPath + @"\dictionaries\js.txt"))
             {
-                jskeys = "(" + reader.ReadToEnd().Replace("\r\n", " |");
+                jskeys = "(" + reader.ReadToEnd().Replace("\r\n", "|");
             }
         }
         public void some_strings_highlight(ref RichTextBox textBox, int index_str, Textfile selected_file)
         {
-            int[] strings = new int[1];
+            int[] strings = { index_str};
             string keywords;
+
 
             if(textBox.Lines.Length - 1 > index_str)
             {
@@ -60,13 +61,20 @@ namespace testApp
                 return;
             }
 
+            keywords += @"|\([^()]*\)|";
+            keywords += "[" + @"\" + "\"'][^" + @"\" + "\"']*[" + @"\" + "\"']|";
+            keywords += @" [^() ]*\(.{0,}\)";
             keywords += ")";
-
+            // for (this text) \([^()]*\)
+            // for 'this' or "this" text [\"'][^\"']*[\"']
+            //  for titles [^() ]*\(.{0,}\)
+            Console.WriteLine("Indexes:" + strings.Length + " Index str " + index_str + " lines " + textBox.Lines.Length);
             foreach(int ln_index in strings)
             {
                 foreach(Match match in Regex.Matches(textBox.Lines[ln_index], keywords))
                 {
-                    Console.WriteLine("Value:" + match.Value + " index " + match.Index);
+                    Console.WriteLine("Value:" + match.Value + " index " + match.Index + " 123 ");
+                    
                 }
             }
         }
