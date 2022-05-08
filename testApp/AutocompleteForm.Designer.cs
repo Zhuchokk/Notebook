fd6a214479d9b632ff0a2c461e5efe5cbae744ca
Artiom <azhuchok207@gmail.com>
2022-05-08 22:43:34 +0300

    superficial localization for autocompleteform created

--- testApp/AutocompleteForm.Designer.cs
+++ testApp/AutocompleteForm.Designer.cs
@@ -29,6 +29,7 @@ namespace testApp
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AutocompleteForm));
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.numericUpDown1 = new FlatNumericUpDown();
             this.label1 = new System.Windows.Forms.Label();
@@ -48,25 +49,20 @@ namespace testApp
             // 
             // checkBox1
             // 
-            this.checkBox1.Appearance = System.Windows.Forms.Appearance.Button;
+            resources.ApplyResources(this.checkBox1, "checkBox1");
             this.checkBox1.Checked = true;
             this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
             this.checkBox1.FlatAppearance.BorderSize = 0;
-            this.checkBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.checkBox1.Location = new System.Drawing.Point(12, 52);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(18, 17);
-            this.checkBox1.TabIndex = 0;
-            this.checkBox1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.checkBox1.UseVisualStyleBackColor = true;
             this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             this.checkBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.checkBox1_Paint);
             // 
             // numericUpDown1
             // 
+            resources.ApplyResources(this.numericUpDown1, "numericUpDown1");
             this.numericUpDown1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.numericUpDown1.ButtonHighlightColor = System.Drawing.Color.Maroon;
-            this.numericUpDown1.Location = new System.Drawing.Point(119, 83);
             this.numericUpDown1.Maximum = new decimal(new int[] {
             2000,
             0,
@@ -78,8 +74,6 @@ namespace testApp
             0,
             0});
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(65, 22);
-            this.numericUpDown1.TabIndex = 1;
             this.numericUpDown1.Value = new decimal(new int[] {
             400,
             0,
@@ -89,26 +83,18 @@ namespace testApp
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 85);
+            resources.ApplyResources(this.label1, "label1");
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(104, 17);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Appear Interval";
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 120);
+            resources.ApplyResources(this.label2, "label2");
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(198, 17);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Min fragment length for popup";
             // 
             // numericUpDown2
             // 
+            resources.ApplyResources(this.numericUpDown2, "numericUpDown2");
             this.numericUpDown2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.numericUpDown2.Location = new System.Drawing.Point(213, 120);
             this.numericUpDown2.Maximum = new decimal(new int[] {
             20,
             0,
@@ -120,8 +106,6 @@ namespace testApp
             0,
             0});
             this.numericUpDown2.Name = "numericUpDown2";
-            this.numericUpDown2.Size = new System.Drawing.Size(65, 22);
-            this.numericUpDown2.TabIndex = 4;
             this.numericUpDown2.Value = new decimal(new int[] {
             2,
             0,
@@ -131,87 +115,59 @@ namespace testApp
             // 
             // checkBox2
             // 
-            this.checkBox2.Appearance = System.Windows.Forms.Appearance.Button;
+            resources.ApplyResources(this.checkBox2, "checkBox2");
             this.checkBox2.Checked = true;
             this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
             this.checkBox2.FlatAppearance.BorderSize = 0;
-            this.checkBox2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.checkBox2.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.checkBox2.Location = new System.Drawing.Point(12, 25);
             this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(18, 17);
-            this.checkBox2.TabIndex = 5;
             this.checkBox2.UseVisualStyleBackColor = true;
             this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
             this.checkBox2.Paint += new System.Windows.Forms.PaintEventHandler(this.checkBox1_Paint);
             // 
             // groupBox1
             // 
+            resources.ApplyResources(this.groupBox1, "groupBox1");
             this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Location = new System.Drawing.Point(358, 25);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(226, 117);
-            this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "About";
             // 
             // label3
             // 
-            this.label3.Location = new System.Drawing.Point(7, 18);
+            resources.ApplyResources(this.label3, "label3");
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(213, 96);
-            this.label3.TabIndex = 0;
-            this.label3.Text = "To use autocomplete, check the box next to \"autocomplete enabled\". To auto-spawn " +
-    "check the box \"Auto Popup\", else use Ctrl-Space\r\n";
             // 
             // label4
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 162);
+            resources.ApplyResources(this.label4, "label4");
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(71, 17);
-            this.label4.TabIndex = 8;
-            this.label4.Text = "Dictionary";
             // 
             // label5
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(34, 25);
+            resources.ApplyResources(this.label5, "label5");
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(150, 17);
-            this.label5.TabIndex = 9;
-            this.label5.Text = "Autocomplete Enabled";
             // 
             // label6
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(37, 52);
+            resources.ApplyResources(this.label6, "label6");
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(82, 17);
-            this.label6.TabIndex = 10;
-            this.label6.Text = "Auto Popup";
             // 
             // comboBox1
             // 
+            resources.ApplyResources(this.comboBox1, "comboBox1");
             this.comboBox1.BorderColor = System.Drawing.Color.RoyalBlue;
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
-            "User-Russian",
-            "User-English",
-            "User-All",
-            "Programmer"});
-            this.comboBox1.Location = new System.Drawing.Point(105, 159);
+            resources.GetString("comboBox1.Items"),
+            resources.GetString("comboBox1.Items1"),
+            resources.GetString("comboBox1.Items2"),
+            resources.GetString("comboBox1.Items3")});
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(173, 24);
-            this.comboBox1.TabIndex = 11;
-            this.comboBox1.Text = "User-English";
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // AutocompleteForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(601, 203);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -224,13 +180,10 @@ namespace testApp
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.checkBox1);
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(619, 250);
             this.MinimizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(619, 250);
             this.Name = "AutocompleteForm";
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
-            this.Text = "Autocomplete";
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
             this.groupBox1.ResumeLayout(false);
